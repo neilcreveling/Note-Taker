42cab26864ea20518b73c59d10010b4e0848bca0
Neil Creveling <neil.creveling@gmail.com>
2021-04-13 13:34:47 -0400

    set up the express app to handle data parsing

--- server.js
+++ server.js
@@ -10,3 +10,7 @@ const { v4: uuidv4 } = require('uuid');
 // sets up express app
 const app = express()
 const PORT = process.env.PORT || 8000;
+
+// sets up the express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
