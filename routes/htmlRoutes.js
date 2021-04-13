a9f1562a066f331e9da76fcc4d4b7b9f612037f3
Neil Creveling <neil.creveling@gmail.com>
2021-04-13 14:09:58 -0400

    create html route paths in htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,1 +1,14 @@
+// dependencies
+const path = require('path');
 
+// routes
+module.exports = (app) => {
+
+    app.get('/', (req, res) => 
+        res.sendFile(path.join(__dirname, './public/index.html'))
+    );
+
+    app.get('/notes', (req, res) =>
+        res.sendFile(path.join(__dirname, './public/notes.html'))
+    ;)
+}
